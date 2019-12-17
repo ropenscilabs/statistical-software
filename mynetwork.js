a6f8e18ba58097918b92562ab869d5cd6b573aac
mpadge <mark.padgham@email.com>
2019-12-17 12:35:05 +0100

    yet more:rocket:network

--- mynetwork.js
+++ mynetwork.js
@@ -23,6 +23,7 @@ var
 // 143: Dynamic testing
 // 144: Exhaustive testing
 // 150: How many testers/users?
+// 160: Program Graphs and Paths
 //
 // 2: Review
 // 21: Start and End
@@ -30,13 +31,37 @@ var
 // 3: Software Life Cycles
 // 31: Retirement
 // 32: Transfer
+//
 // 4: Metrics
 // 41: Documentation
 // 42: Meta
 // 421: Reputation
 // 422: Breadth of Use
+// 43: Formal CS Metrics
+// 431: Fault Proneness
+// 432: Fault Detectability
+// 433: Error Detectability
+// 434: Error Maskability
+// 435: Failure Avoidance
+// 436: Failure Tolerance
+// 4311: Cyclomatic Complexity
+// 4312: Volume
+//
 // 5: Design
 // 51: Planning
+// 52: Useability
+// 521: Ease of Use
+// 522: Ease of Learning
+// 523: Customizability
+// 524: Calibrability
+// 525: Interoperability
+// 53: Structure
+// 531: Design Integrity
+// 532: Modularity
+// 5321: Cohesion
+// 5322: Coupling
+// 533: Testability
+// 534: Adaptability
 var nodes = new vis.DataSet([
   { id: 0,
     group: 0,
@@ -85,6 +110,7 @@ var nodes = new vis.DataSet([
   { id: 144, group: 11, label: "Exhaustive Testing", title: "When you finish module-testing a program,<br>you have really only just begun the testing process<br>(Meyers &c, 2011)" },
 
   { id: 150, group: 1, label: "How Many Users / Testers?", title: "Empirical formula from Meyers &c (2011):<br>Generally not so many" },
+  { id: 160, group: 11, label: "Program Graphs and Paths", title: "... and all of the CS stuff that goes along with<br>formal treatments of testing" },
 
   { id: 2, label: "Review", shape: "circle", group: 2},
   { id: 21, label: "Start and End", group: 2, title: "A critical component that is a fundmental part of<br>the FDA's 'Good Practices in Software Validation'"},
@@ -105,9 +131,31 @@ var nodes = new vis.DataSet([
   { id: 42, label: "Meta", group: 4 },
   { id: 421, label: "Reputation", group: 4 },
   { id: 422, label: "Breadth of Use", group: 4 },
+  { id: 43, label: "Formal CS Metrics", group: 4 },
+  { id: 431, label: "Fault Proneness", group: 4 },
+  { id: 432, label: "Fault Detectability", group: 4 },
+  { id: 433, label: "Error Detectability", group: 4 },
+  { id: 434, label: "Error Maskability", group: 4 },
+  { id: 435, label: "Failure Avoidance", group: 4 },
+  { id: 436, label: "Failure Tolerance", group: 4 },
+  { id: 4311, label: "Cyclomatic Complexity", group: 4 },
+  { id: 4312, label: "Volume", group: 4 },
 
   { id: 5, label: "Design", group: 5},
-  { id: 51, label: "Planning", group: 5}
+  { id: 51, label: "Planning", group: 5},
+  { id: 52, label: "Useability", group: 5},
+  { id: 521, label: "Ease of Use", group: 5},
+  { id: 522, label: "Ease of Learning", group: 5},
+  { id: 523, label: "Customizability", group: 5},
+  { id: 524, label: "Calibrability", group: 5},
+  { id: 525, label: "Interoperability", group: 5},
+  { id: 53, label: "Structure", group: 5},
+  { id: 531, label: "Design Integrity", group: 5},
+  { id: 532, label: "Modularity", group: 5},
+  { id: 5321, label: "Cohesion", group: 5},
+  { id: 5322, label: "Coupling", group: 5},
+  { id: 533, label: "Testability", group: 5},
+  { id: 534, label: "Adaptability", group: 5}
 
   /*
    {
@@ -152,6 +200,7 @@ var nodes = new vis.DataSet([
 // 143: Dynamic testing
 // 144: Exhaustive testing
 // 150: How many testers/users?
+// 160: Program Graphs and Paths
 //
 // 2: Review
 // 21: Start and End
@@ -159,13 +208,37 @@ var nodes = new vis.DataSet([
 // 3: Software Life Cycles
 // 31: Retirement
 // 32: Transfer
+//
 // 4: Metrics
 // 41: Documentation
 // 42: Meta
 // 421: Reputation
 // 422: Breadth of Use
+// 43: Formal CS Metrics
+// 431: Fault Proneness
+// 432: Fault Detectability
+// 433: Error Detectability
+// 434: Error Maskability
+// 435: Failure Avoidance
+// 436: Failure Tolerance
+// 4311: Cyclomatic Complexity
+// 4312: Volume
+//
 // 5: Design
 // 51: Planning
+// 52: Useability
+// 521: Ease of Use
+// 522: Ease of Learning
+// 523: Customizability
+// 524: Calibrability
+// 525: Interoperability
+// 53: Structure
+// 531: Design Integrity
+// 532: Modularity
+// 5321: Cohesion
+// 5322: Coupling
+// 533: Testability
+// 534: Adaptability
 var edges = new vis.DataSet([
   { from: 1, to: 0, arrows: "to", value: 1 },
   { from: 101, to: 1, arrows: "to" },
@@ -196,13 +269,15 @@ var edges = new vis.DataSet([
   { from: 150, to: 1, arrows: "to" },
 
   { from: 142, to: 143, arrows: "to, from" },
-  { from: 140, to: 141, arrows: "to, from" },
+  { from: 140, to: 141, arrows: "to, from", label: "No difference,really" },
   { from: 140, to: 142, arrows: "to, from", dashes: [10, 10] },
   { from: 140, to: 143, arrows: "to, from", dashes: [10, 10] },
   { from: 141, to: 142, arrows: "to, from", dashes: [10, 10] },
   { from: 141, to: 143, arrows: "to, from", dashes: [10, 10] },
   { from: 144, to: 140, arrows: "to" },
   { from: 144, to: 143, arrows: "to" },
+
+  { from: 160, to: 11, arrows: "to", value: 1 },
 
   { from: 21, to: 2, arrows: "to" },
   { from: 22, to: 2, arrows: "to, from" },
@@ -238,8 +313,31 @@ var edges = new vis.DataSet([
   { from: 42, to: 4, arrows: "to" },
   { from: 421, to: 42, arrows: "to" },
   { from: 422, to: 42, arrows: "to" },
+  { from: 43, to: 4, arrows: "to" },
+  { from: 431, to: 43, arrows: "to" },
+  { from: 432, to: 43, arrows: "to" },
+  { from: 433, to: 43, arrows: "to" },
+  { from: 434, to: 43, arrows: "to" },
+  { from: 435, to: 43, arrows: "to" },
+  { from: 436, to: 43, arrows: "to" },
+  { from: 4311, to: 431, arrows: "to" },
+  { from: 4312, to: 431, arrows: "to" },
 
-  { from: 51, to: 5, arrows: "to", dashes: [10, 10] }
+  { from: 51, to: 5, arrows: "to", dashes: [10, 10] },
+  { from: 52, to: 5, arrows: "to" },
+  { from: 521, to: 52, arrows: "to" },
+  { from: 522, to: 52, arrows: "to" },
+  { from: 523, to: 52, arrows: "to" },
+  { from: 524, to: 52, arrows: "to" },
+  { from: 525, to: 52, arrows: "to" },
+  { from: 53, to: 5, arrows: "to" },
+  { from: 531, to: 53, arrows: "to" },
+  { from: 532, to: 53, arrows: "to" },
+  { from: 5321, to: 532, arrows: "to" },
+  { from: 5322, to: 532, arrows: "to" },
+  { from: 533, to: 53, arrows: "to" },
+  { from: 533, to: 1, arrows: "to" },
+  { from: 534, to: 53, arrows: "to" }
 ]);
 
 var container = document.getElementById("mynetwork");
